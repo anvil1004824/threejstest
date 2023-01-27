2c1c448031348899e644bf48f2a9568c574ae434
anvil1004824 <anvil1004824@gmail.com>
2023-01-27 19:02:18 +0900

    Annotation Added

--- attempt1.js
+++ attempt1.js
@@ -1,20 +1,25 @@
+//Three.js 라이브러리 import
 import * as THREE from "three";
 
-// Initialize variables.
-var scene, camera, renderer, WIDTH, HEIGHT;
-var PI = Math.PI;
-var t = 0;
-var a = 10;
-var legup;
-var cos = Math.cos;
-var sin = Math.sin;
+//기본 변수 설정
+let scene, camera, renderer, WIDTH, HEIGHT;
+const PI = Math.PI;
+let t = 0;
+const a = 10;
+let legup;
+const cos = Math.cos;
+const sin = Math.sin;
 
+//기본 설정
 function init(event) {
-  var container = document.getElementById("world");
+  //HTML 요소 불러오기
+  const container = document.getElementById("world");
 
+  //캔버스의 높이와 너비 지정
   HEIGHT = window.innerHeight;
   WIDTH = window.innerWidth;
 
+  //장면과 카메라 설정
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(50, WIDTH / HEIGHT, 1, 2000);
   camera.position.z = 50;
@@ -24,21 +29,26 @@ function init(event) {
   renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
   container.appendChild(renderer.domElement);
 
-  var geom = new THREE.BoxGeometry(4, a, 4);
-  var material = new THREE.MeshPhongMaterial({
+  //legup 설정
+  const geom = new THREE.BoxGeometry(4, a, 4);
+  const material = new THREE.MeshPhongMaterial({
     color: 0xdddddd,
     specular: 0x009900,
     shininess: 30,
     flatShading: true,
   });
   legup = new THREE.Mesh(geom, material);
+
+  //장면에 legup 추가
   scene.add(legup);
 
-  var globalLight = new THREE.AmbientLight(0xffffff, 1);
+  //글로벌 라이트 추가
+  const globalLight = new THREE.AmbientLight(0xffffff, 1);
   scene.add(globalLight);
 
-  window.addEventListener("resize", handleWindowResize, false);
   loop();
+
+  window.addEventListener("resize", handleWindowResize, false);
 }
 
 function handleWindowResize() {
@@ -49,6 +59,7 @@ function handleWindowResize() {
   camera.updateProjectionMatrix();
 }
 
+//애니메이팅
 function loop() {
   update();
 
@@ -62,7 +73,5 @@ window.addEventListener("load", init, false);
 function update() {
   t += 0.1;
 
-  //legup.position.x = (sin(t) * a) / 4;
-  //legup.position.y = (((-cos(2 * t) + 1) * 0.5 * a) / 2) * (1 - Math.sqrt(3) * 0.5);
   legup.rotation.z = (sin(t) * PI) / 6;
 }
